4dbf6eef8969d44eb0f4295c0073f1326210dc84
pedrolsb <41968886+pedrolsb@users.noreply.github.com>
2018-08-01 09:46:30 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-echo "<h1>Openshift Workshop v2.1</h1> ";
+echo "<h1>Openshift Workshop v3.0</h1> ";
 echo "<h2>Usando o Liveness / Readiness</h2>";
+echo "<h2>WEBHOOKS</h2>";
 echo $_SERVER['SERVER_ADDR'];
 ?>
